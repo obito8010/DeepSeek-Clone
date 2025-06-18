c17b0ef5204bd5f4834eab3b3888f5391ecd82ed
obito8010 <chinmay.saseendran@gmail.com>
2025-06-18 15:48:00 +0530

    finished sidebar component

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -18,8 +18,47 @@ const Sidebar = ({expand,setExpand}) => {
                       </div>
                 </div>
             </div>
+
+            <button className={`mt-8 flex items-center justify-center cursor-pointer ${expand ? "bg-primary hover:opacity-90 rounded-2xl gap-2 p-2.5 w-max" : "group relative h-9 w-9 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+                <Image className={expand ? 'w-6' : 'w-7'} src={expand ? assets.chat_icon : assets.chat_icon_dull} alt=''/>
+                <div className='absolute w-max -top-12 -right-12 opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none'>
+                    New chat
+                    <div className='w-3 h-3 absolute bg-black rotate-45 left-4 -bottom-1.5'></div>
+                </div>
+            {expand && <p className='text-white text font-medium'>New chat</p>}
+            </button>
+
+
+            <div className={`mt-8 text-white/25 text-sm ${expand ? "block" : "hidden"}`}>
+                <p className='my-1'>Recents</p>
+                {/* Chat label 47:57*/}
+
+            </div>
         </div>
+
+
+        <div>
+            <div className={`flex items-center cursor-pointer group relative ${expand ? "gap-1 text-white/80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/80 text-sm p-2.5 border border-primary rounded-lg hover:bg-white/10 cursor-pointer": "h-10 w-10 mx-auto hover:bg-gray-500/30 rounded-lg"}`}>
+                <Image className={expand ? "w-5" : "w-6.5 mx-auto"} src={expand ? assets.phone_icon : assets.phone_icon_dull} alt=''/>
+                <div className={`absolute -top-60 pb-8 ${!expand && "-right-40"} opacity-0 group-hover:opacity-100 hidden group-hover:block transition`}>
+                    <div className='relative w-max bg-black text-white text-sm p-3 rounded-lg shadow-lg'>
+                        <Image src={assets.qrcode} alt='' className='w-44'/>
+                        <p>Scan to get DeepSeek App</p>
+                        <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "right-1/2" : "left-4"} -bottom-1.5`}></div>
+                    </div>
+                </div>
+                {expand && <><span>Get App</span><Image alt='' src={assets.new_icon}/></>}
+            </div>
+
+        <div className={`flex item-center ${expand ? 'hover:bg-white/10 rounded-lg':'justify-center w-full'}gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+            <Image src={assets.profile_icon} alt='' className='w-7'/>
+            {expand && <span>my profile</span>}
+        </div>
+
+        </div>
+        
     </div>
+
   )
 }
 
